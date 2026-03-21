a019ade395ac5921a21ea5e7dc206212792d2a10
Doi_Trong_Canh <doicanh@gmail.com>
2026-03-21 10:01:36 +0700

    feat: deaploy smart contract on SapphireTestnet

--- dapp-be/hardhat.config.ts
+++ dapp-be/hardhat.config.ts
@@ -1,15 +1,43 @@
 import type { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-ethers";
+import "@nomicfoundation/hardhat-chai-matchers";
+import "@nomicfoundation/hardhat-verify";
 import "@oasisprotocol/sapphire-hardhat";
-import "nomicfoundation/hardhat-verify";
 
-import dotenv from "dotenv"
-
+import dotenv from "dotenv";
+dotenv.config();
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.20",
-
-  }
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+      viaIR: true,
+    },
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    sapphireTestnet: {
+      url: "https://testnet.sapphire.oasis.io",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      chainId: 0x5aff, // 0x5aff is 23295 in decimal
+      gasPrice: 100000000000, //  10 gwei
+      timeout: 120000, // 120 seconds
+      httpHeaders: {
+        "User-Agent": "Hardhat",
+      },
+    },
+  },
+  etherscan: {
+    enabled: false,
+  },
+  sourcify: {
+    enabled: true,
+  },
 };
 
 export default config;
